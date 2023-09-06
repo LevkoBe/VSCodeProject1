7eb81c4aa3b38b78a9d065b66689e7b88755939d
LevkoBe <lbeniakh@kse.org.ua>
2023-09-06 23:17:15 +0300

    Aweasome website

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const unlockButton = document.getElementById("unlockButton");
+
+    unlockButton.addEventListener("click", function () {
+        alert("You've unlocked the Mystery Box! 🎉");
+    });
+});
